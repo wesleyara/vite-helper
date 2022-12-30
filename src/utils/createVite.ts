373d9ab79a1911c0875d42fd7f245c31fa603d7a
wesleyara <wes1gom@gmail.com>
2022-12-30 19:47:21 -0300

    fix: change fileRequest

--- src/utils/createVite.ts
+++ src/utils/createVite.ts
@@ -1,9 +1,9 @@
+import { fileRequest } from "down-git-files";
 import fs from "fs";
 import { cd } from "shelljs";
 
 import { silentExec } from ".";
 import { IDependencies, IProjectDetails } from "../@types";
-import { fileRequest } from "./fileRequest";
 
 export const createVite = async ({
   isTypescript,
@@ -31,31 +31,43 @@ export const createVite = async ({
 
   await Promise.all([
     fileRequest({
+      user: "vite-helper",
+      repo: "vite-helper",
       path: "templates/viteTemplate/vercel.json",
       branch: "main",
       file: "vercel.json",
     }),
     fileRequest({
+      user: "vite-helper",
+      repo: "vite-helper",
       path: `templates/viteTemplate/.gitattributes`,
       branch: "main",
       file: ".gitattributes",
     }),
     fileRequest({
+      user: "vite-helper",
+      repo: "vite-helper",
       path: `templates/viteTemplate/${folder}/${viteConfigName}`,
       branch: "main",
       file: viteConfigName,
     }),
     fileRequest({
+      user: "vite-helper",
+      repo: "vite-helper",
       path: "templates/viteTemplate/_redirects",
       branch: "main",
       file: "public/_redirects",
     }),
     fileRequest({
+      user: "vite-helper",
+      repo: "vite-helper",
       path: `templates/viteTemplate/${folder}/main.${extension}`,
       branch: "main",
       file: `src/main.${extension}`,
     }),
     fileRequest({
+      user: "vite-helper",
+      repo: "vite-helper",
       path: `templates/viteTemplate/${folder}/index.${extension}`,
       branch: "main",
       file: `src/pages/Home/index.${extension}`,
@@ -63,6 +75,8 @@ export const createVite = async ({
 
     isTypescript
       ? fileRequest({
+          user: "vite-helper",
+          repo: "vite-helper",
           path: `templates/viteTemplate/${folder}/vite-env.d.ts`,
           branch: "main",
           file: "src/vite-env.d.ts",
